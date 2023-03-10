76d9a75303b27e2d68d2f6b721ffd41a64a8bf3e
NickC <nick.fiona@virginmedia.com>
2023-03-10 11:48:20 +0000

    build error fix

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
-import Link from "./link";
+import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
